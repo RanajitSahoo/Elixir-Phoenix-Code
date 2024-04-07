8f9f74ca7be68d2aeca39407a40eec0ce8a70228
RanajitSahoo <ranajitsahoo170597@gmail.com>
2024-04-07 19:51:02 +0530

    createing model shemaa table

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -6,4 +6,14 @@ defmodule ForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  @spec users(Plug.Conn.t(),any()):: Plug.Conn.t()
+  def users(conn,_params) do
+    users = [
+      %{id: 1,name: "Raja", email: "raja@gmail.com"},
+      %{id: 2,name: "Abdul", email: "abdul@gmail.com"}
+    ]
+    # render(conn, :users,users: users ,layout: false)
+    json(conn, %{users: users})
+  end
 end
